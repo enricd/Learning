6df7108b9ac480cacc2a01542f0d14a9370920d8
enricd <edomingodomenech@gmail.com>
2025-02-10 00:28:58 +0100

    init ai image fastapi tutorial

---
+++ FastAPI/ai_photo_generator_fastapi/README.md
@@ -0,0 +1,2 @@
+from youtube.com/@CodingEntrepreneurs
+https://www.youtube.com/watch?v=lV82LDKT16A
---
+++ FastAPI/ai_photo_generator_fastapi/hello.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from ai-photo-generator-fastapi!")
+
+
+if __name__ == "__main__":
+    main()
