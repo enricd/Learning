3794a591ac3520506a1f44060e9b6e437c9afa66
enricd <edomingodomenech@gmail.com>
2025-02-25 01:06:20 +0100

    add rate limiter

--- FastAPI/ai_photo_generator_fastapi/README.md
+++ FastAPI/ai_photo_generator_fastapi/README.md
@@ -1,2 +1,9 @@
 from youtube.com/@CodingEntrepreneurs
 https://www.youtube.com/watch?v=lV82LDKT16A
+
+
+
+$ cd src
+$ uv run fastapi dev main.py   
+
+$ uv run fastapi run   # for production, and better use gunicorn with uvicorn
--- FastAPI/ai_photo_generator_fastapi/src/main.py
+++ FastAPI/ai_photo_generator_fastapi/src/main.py
@@ -1,18 +1,31 @@
 from typing import Union
+from contextlib import asynccontextmanager
 
 from decouple import config
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Depends
+from fastapi_limiter import FastAPILimiter
+from fastapi_limiter.depends import RateLimiter
 from pydantic import BaseModel
+import redis.asyncio as redis
 
 import helpers
 
 
 REDIS_URL = config("REDIS_URL")
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(_: FastAPI):
+    redis_conn = redis.from_url(REDIS_URL)
+    await FastAPILimiter.init(
+        redis_conn
+    )
+    yield
+    await FastAPILimiter.close()
+
+app = FastAPI(lifespan=lifespan)
 
 
-@app.get("/")
+@app.get("/", dependencies=[Depends(RateLimiter(times=2, seconds=10))])
 def read_root():
     return {"Hello": "World"}
 
@@ -22,7 +35,7 @@ class ImageGenertionRequest(BaseModel):
 
 
 
-@app.post("/generate")
+@app.post("/generate", dependencies=[Depends(RateLimiter(times=2, seconds=10))])
 def create_image(data: ImageGenertionRequest):
     try: 
         pred_result = helpers.generate_image(data.prompt)
